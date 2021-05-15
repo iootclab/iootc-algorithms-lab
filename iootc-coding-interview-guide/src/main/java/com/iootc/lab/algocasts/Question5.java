ec101171d545f4afe9de9430bd44a9e8d998e7c7
iootclab <johnathon.btc@gmail.com>
2021-05-15 20:18:14 +0800

    feat:  two number sum with non-operators [no.6];

--- iootc-coding-interview-guide/src/main/java/com/iootc/lab/algocasts/Question5.java
+++ iootc-coding-interview-guide/src/main/java/com/iootc/lab/algocasts/Question5.java
@@ -1,42 +1,34 @@
 package com.iootc.lab.algocasts;
 
-import com.google.common.collect.Sets;
-
-import java.util.Set;
-
 /**
- * P5.单身数字
- * <p>题目：给你一个非空的整数数组，这个数组中有一个整数只出现了一次，其它的整数都出现两次，你要找出这个只出现一次的整数。<br/>
- * <p>描述：1. 比如说，给你的数组是：5, 7, 5, 6, 6，这里 7 只出现了一次，因此你要返回的就是 7。
+ * P5.不用+/-求两数之和
+ * <p>题目：给你两个整数，在不使用 +/- 这两个运算符的前提下，求它们的和。<br/>
+ * <p>描述：1. 比如说，给你的数组是：18、25，不用运算符求出它们之和。
  *
  * @author Johnathon Yuan
- * @since 2021-05-13 23:40
+ * @since 2021-05-15 20:12
  */
 public class Question5 {
 
-    // Time: O(n), Space: O(n)
-    public int isSingleNumberWithSet(int[] nums) {
-        Set<Integer> uniqueSet = Sets.newHashSet();
-        int sum = 0;
-        int uniqueSum = 0;
-        for (int num : nums) {
-            if (!uniqueSet.contains(num)) {
-                uniqueSum += num;
-                uniqueSet.add(num);
-            }
-            sum += num;
-        }
-        return 2 * uniqueSum - sum;
+    public int getSumRecursive(int a, int b) {
+        return b == 0 ? a : getSumRecursive(a^b, (a&b) << 1);
     }
 
-    // Time: O(n), Space: O(1)
-    public int isSingleNumberWithXOR(int[] nums) {
-        int result = 0;
-        for (int num : nums) {
-            result ^= num;
+    // Time: O(m), Space: O(1)
+    public int getSumIterative(int a, int b) {
+        while (b != 0) {
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            a = sum;
+            b = carry;
         }
-        return result;
+        return a;
     }
 
+    public static void main(String[] args) {
+        Question5 question = new Question5();
 
+        System.out.println(question.getSumRecursive(18, 25));
+        System.out.println(question.getSumIterative(18, 25));
+    }
 }
---
+++ iootc-coding-interview-guide/src/main/java/com/iootc/lab/algocasts/Question6.java
@@ -0,0 +1,42 @@
+package com.iootc.lab.algocasts;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+/**
+ * P6.单身数字
+ * <p>题目：给你一个非空的整数数组，这个数组中有一个整数只出现了一次，其它的整数都出现两次，你要找出这个只出现一次的整数。<br/>
+ * <p>描述：1. 比如说，给你的数组是：5, 7, 5, 6, 6，这里 7 只出现了一次，因此你要返回的就是 7。
+ *
+ * @author Johnathon Yuan
+ * @since 2021-05-13 23:40
+ */
+public class Question6 {
+
+    // Time: O(n), Space: O(n)
+    public int isSingleNumberWithSet(int[] nums) {
+        Set<Integer> uniqueSet = Sets.newHashSet();
+        int sum = 0;
+        int uniqueSum = 0;
+        for (int num : nums) {
+            if (!uniqueSet.contains(num)) {
+                uniqueSum += num;
+                uniqueSet.add(num);
+            }
+            sum += num;
+        }
+        return 2 * uniqueSum - sum;
+    }
+
+    // Time: O(n), Space: O(1)
+    public int isSingleNumberWithXOR(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+
+}
